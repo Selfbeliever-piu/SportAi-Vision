7b1cd9c12386a4d46e8e10dbb4ac33adebf8494a
piu <mspriyankathorat@gmail.com>
2025-03-31 12:55:50 +0530

    changes for the dev app

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Routes
 app.use('/poseEstimate', vidoeProcessingRouter);
 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require("./app"); // 👈 Import the app
-require("dotenv").config();
+const app = require("./app");
+const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+  });
+
+dotenv.config({ path: './.env' });
 const PORT = process.env.PORT || 5000;
 
 // Start the server
